036a1a8f47f605ae77d62edd5bdc35af4178275a
Marc Paquette <marc.paquette@cockroachlabs.com>
2025-06-27 12:52:13 -0400

    Cleaning up scripts and files

--- init_visus.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kubectl exec -it my-release-cockroachdb-0  -c visus -- visus \
-      init \
-      --url \
-      "postgres://root@localhost:26257/defaultdb?application_name=visus&sslmode=require&ssrootcert=/cockroach/client/ca.crt&sslcert=/cockroach/client/client.root.crt&sslkey=/cockroach/client/client.root.key"
--- load_metrics.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-for CRDB_NODE in $(kubectl get pods -o json | jq -r '.items[] | select(.metadata.name | test("^my-release-cockroachdb-[0-9]")) | .metadata.name')
-do 
-    kubectl exec -it $CRDB_NODE -c visus  -- visus \
-        --url "postgres://root@localhost:26257/defaultdb?application_name=visus&sslmode=require&ssrootcert=/cockroach/client/ca.crt&sslcert=/cockroach/client/client.root.crt&sslkey=/cockroach/client/client.root.key" \
-        collection put --yaml - < query_count.yaml
-    done
-
