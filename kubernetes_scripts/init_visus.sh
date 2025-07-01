f7bbf5fb6d48b482de8faf25ea89b07a272b2d88
Marc Paquette <marc.paquette@cockroachlabs.com>
2025-07-01 14:42:26 -0400

    Fix typo in script

--- kubernetes_scripts/init_visus.sh
+++ kubernetes_scripts/init_visus.sh
@@ -2,4 +2,4 @@
 kubectl exec -it my-release-cockroachdb-0  -c visus -- visus \
       init \
       --url \
-      "postgres://root@localhost:26257/defaultdb?application_name=visus&sslmode=require&ssrootcert=/cockroach/client/ca.crt&sslcert=/cockroach/client/client.root.crt&sslkey=/cockroach/client/client.root.key"
+      "postgres://root@localhost:26257/defaultdb?application_name=visus&sslmode=require&sslrootcert=/cockroach/client/ca.crt&sslcert=/cockroach/client/client.root.crt&sslkey=/cockroach/client/client.root.key"
--- kubernetes_scripts/test_metrics.sh
+++ kubernetes_scripts/test_metrics.sh
@@ -3,7 +3,7 @@ for CRDB_NODE in $(kubectl get pods -o json | jq -r '.items[] | select(.metadata
 do 
     echo 📈Testing node $CRDB_NODE metrics📈
     kubectl exec -it $CRDB_NODE -c visus  -- visus \
-        --url "postgres://root@localhost:26257/defaultdb?application_name=visus&sslmode=require&ssrootcert=/cockroach/client/ca.crt&sslcert=/cockroach/client/client.root.crt&sslkey=/cockroach/client/client.root.key" \
+        --url "postgres://root@localhost:26257/defaultdb?application_name=visus&sslmode=require&sslrootcert=/cockroach/client/ca.crt&sslcert=/cockroach/client/client.root.crt&sslkey=/cockroach/client/client.root.key" \
  	collection test query_count --count 2
     echo 📈Testing Complete📈
 done
